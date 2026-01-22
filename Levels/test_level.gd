d9b2dc8ec64d6c093d68e8dde56ae54672ac857a
dodo3142 <85135076+dodo3142@users.noreply.github.com>
2026-01-22 18:22:17 +0200

    Refactor multiplayer lobby and player spawning logic
    Rewrote test_level.gd to improve lobby creation, joining, and player spawning for Steam multiplayer. Added explicit host/client logic, clearer lobby response handling, and direct player instantiation. Deprecated old player_spawner.gd logic in favor of new approach. Added temporary scene files for test_level.

--- Levels/test_level.gd
+++ Levels/test_level.gd
@@ -5,83 +5,119 @@ extends Node3D
 @onready var player_spawner: MultiplayerSpawner = $PlayerSpawner
 @onready var lobby_id_text: LineEdit = $CanvasLayer/SteamMulti/MarginContainer/VBoxContainer/AddresEntery
 
+@export var player_scene : PackedScene
+@export var spawn_loc : Node3D
+
 var lobby_ID : int = 0
 
 var lobby_created : bool = false
 
-var peer : SteamMultiplayerPeer
+var peer : SteamMultiplayerPeer = SteamMultiplayerPeer.new()
 
 func _ready() -> void:
-	peer =  SteamManger.peer
-	Steam.initRelayNetworkAccess()
-	Steam.lobby_created.connect(_on_lobby_created)
-	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	Steam.lobby_created.connect(_on_lobby_created.bind())
 
-
-
-func _process(delta: float) -> void:
-	pass
-
-func _on_host_button_pressed() -> void:
-	if lobby_created:
-		return
+func become_host():
+	print("Starting host!")
 	
-	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC,3)
+	multiplayer.peer_connected.connect(_add_player_to_game)
+	multiplayer.peer_disconnected.connect(_del_player)
 	
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, 4)
 
-func _on_join_button_pressed() -> void:
-	join_lobby(int(lobby_id_text.text))
+func join_as_client(lobby_id):
+	print("Joining lobby %s" % lobby_id)
+	Steam.lobby_joined.connect(_on_lobby_joined.bind())
+	Steam.joinLobby(int(lobby_id))
 
-func _on_refresh_lobbies_pressed() -> void:
-	var lobbies_btn = lobbies_list.get_children()
-	for i in lobbies_btn:
-		i.queue_free()
+func _on_lobby_created(connect: int, lobby_id):
+	print("On lobby created")
+	if connect == 1:
+		lobby_ID = lobby_id
+		print("Created lobby: %s" % lobby_ID)
+		
+		Steam.setLobbyJoinable(lobby_ID, true)
+		
+		Steam.setLobbyData(lobby_ID, "name", "Lol123")
+		
+		_create_host()
+
+func _create_host():
+	print("Create Host")
 	
-	open_lobby_list()
+	var error = peer.create_host(0)
+	
+	if error == OK:
+		multiplayer.set_multiplayer_peer(peer)
+		
+		if not OS.has_feature("dedicated_server"):
+			_add_player_to_game(1)
+	else:
+		print("error creating host: %s" % str(error))
 
+func _on_lobby_joined(lobby: int, permissions: int, locked: bool, response: int):
+	print("On lobby joined: %s" % response)
+	
+	if response == 1:
+		var id = Steam.getLobbyOwner(lobby)
+		if id != Steam.getSteamID():
+			print("Connecting client to socket...")
+			connect_socket(id)
+	else:
+		# Get the failure reason
+		var FAIL_REASON: String
+		match response:
+			2:  FAIL_REASON = "This lobby no longer exists."
+			3:  FAIL_REASON = "You don't have permission to join this lobby."
+			4:  FAIL_REASON = "The lobby is now full."
+			5:  FAIL_REASON = "Uh... something unexpected happened!"
+			6:  FAIL_REASON = "You are banned from this lobby."
+			7:  FAIL_REASON = "You cannot join due to having a limited account."
+			8:  FAIL_REASON = "This lobby is locked or disabled."
+			9:  FAIL_REASON = "This lobby is community locked."
+			10: FAIL_REASON = "A user in the lobby has blocked you from joining."
+			11: FAIL_REASON = "A user you have blocked is in the lobby."
+		print(FAIL_REASON)
 
+func connect_socket(steam_id: int):
+	var error = peer.create_client(steam_id, 0)
+	if error == OK:
+		print("Connecting peer to host...")
+		multiplayer.set_multiplayer_peer(peer)
+	else:
+		print("Error creating client: %s" % str(error))
 
-func open_lobby_list():
+func list_lobbies():
 	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_WORLDWIDE)
+	# NOTE: If you are using the test app id, you will need to apply a filter on your game name
+	# Otherwise, it may not show up in the lobby list of your clients
 	Steam.requestLobbyList()
 
-func _on_lobby_created(connect: int, _lobby_id: int):
-	if connect:
-		lobby_ID = _lobby_id
-		Steam.setLobbyData(lobby_ID,"name",str(SteamManger.SteamUserName + "'s Lobby"))
-		Steam.setLobbyJoinable(lobby_ID,true)
-		
-		SteamManger.Lobby_ID = lobby_ID
-		SteamManger.isLobbyHost = true
-		print(lobby_ID)
-		hide_menu()
-		peer = SteamMultiplayerPeer.new()
-		peer.server_relay = true
-		peer.create_host()
-		multiplayer.multiplayer_peer = peer
-		player_spawner.spawn_host()
+func _add_player_to_game(id: int):
+	print("Player %s joined the game!" % id)
+	
+	var player_to_add = player_scene.instantiate()
+	player_to_add.set_multiplayer_authority(id)
+	player_to_add.name = str(id)
+	
+	spawn_loc.add_child(player_to_add, true)
+
+func _del_player(id: int):
+	print("Player %s left the game!" % id)
+	if not spawn_loc.has_node(str(id)):
+		return
+	spawn_loc.get_node(str(id)).queue_free()
 
 
-func _on_lobby_match_list(lobbies: Array):
-	for lobby in lobbies:
-		var lobby_name = Steam.getLobbyData(lobby,"name")
-		var mebers_count = Steam.getNumLobbyMembers(lobby)
-		var max_players = Steam.getLobbyMemberLimit(lobby)
-		
-		var btn = Button.new()
-		btn.text = "{0} | {1}/{2}".format([lobby_name,mebers_count,max_players])
-		btn.set_size(Vector2(40,100))
-		
-		btn.pressed.connect(join_lobby.bind(lobby))
-		lobbies_list.add_child(btn)
+func _on_host_button_pressed() -> void:
+	become_host()
+	$CanvasLayer/SteamMulti.visible = false
 
-func join_lobby(_Lobby_id):
-	Steam.joinLobby(_Lobby_id)
-	peer = SteamMultiplayerPeer.new()
-	peer.server_relay = true
-	multiplayer.multiplayer_peer = peer
-	player_spawner.spawn(SteamManger.Steam_ID)
-	hide_menu()
+func _on_join_button_pressed() -> void:
+	join_as_client(int(lobby_id_text.text))
+	$CanvasLayer/SteamMulti.visible = false
 
-func hide_menu():
-	multiplayer_ui.hide()
+func _on_refresh_lobbies_pressed() -> void:
+	list_lobbies()
+	$CanvasLayer/SteamMulti.visible = false
--- Scripts/player_spawner.gd
+++ Scripts/player_spawner.gd
@@ -4,27 +4,27 @@ extends MultiplayerSpawner
 
 var Players = {}
 
-func _ready() -> void:
-	spawn_function = spawn_player
-	if is_multiplayer_authority():
-		multiplayer.peer_connected.connect(spawn)
-		multiplayer.peer_disconnected.connect(remove_player)
-		#call_deferred("spawn_host")
-
-func spawn_host():
-	if is_multiplayer_authority():
-		spawn(1)
-
-func spawn_player(data):
-	var p = PlayerScene.instantiate()
-	p.set_multiplayer_authority(data)
-	Players[data] = p
-	print(Players)
-	return p
-
-func remove_player(data):
-	Players[data].queue_free()
-	Players.erase(data)
-
-func _process(delta: float) -> void:
-	pass
+#func _ready() -> void:
+	#spawn_function = spawn_player
+	#if is_multiplayer_authority():
+		#multiplayer.peer_connected.connect(spawn)
+		#multiplayer.peer_disconnected.connect(remove_player)
+		##call_deferred("spawn_host")
+#
+#func spawn_host():
+	#if is_multiplayer_authority():
+		#spawn(1)
+#
+#func spawn_player(data):
+	#var p = PlayerScene.instantiate()
+	#p.set_multiplayer_authority(data)
+	#Players[data] = p
+	#print(Players)
+	#return p
+#
+#func remove_player(data):
+	#Players[data].queue_free()
+	#Players.erase(data)
+#
+#func _process(delta: float) -> void:
+	#pass
